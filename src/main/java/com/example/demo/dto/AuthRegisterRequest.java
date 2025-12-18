f4fa9ae46d3cee04fc06dbb912060f1e109bb1dc
bala-amypo <bala@amypo.in>
2025-12-18 06:18:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthRegisterRequest.java
@@ -0,0 +1,26 @@
+package com.example.demo.dto;
+
+public class AuthRegisterRequest {
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    public AuthRegisterRequest() {
+    }
+
+    public AuthRegisterRequest(String fullName, String email, String password) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
---
+++ src/main/java/com/example/demo/dto/EmployeeSearchRequest.java
@@ -0,0 +1,31 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class EmployeeSearchRequest {
+
+    private List<String> skills;
+    private Long userId;
+
+    public EmployeeSearchRequest() {
+    }
+
+    public EmployeeSearchRequest(List<String> skills, Long userId) {
+        this.skills = skills;
+        this.userId = userId;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
